dda7992018a8eb8ec6940ad04861a83840eb4d37
pedrogussi <pedrogussi@hotmail.com>
2021-02-10 14:57:44 -0300

    post-creation

--- src/componets/Form/Form.js
+++ src/componets/Form/Form.js
@@ -2,7 +2,10 @@ import React, { useState } from 'react';
 
 import { TextField, Button, Typography, Paper } from '@material-ui/core';
 import FileBase from 'react-file-base64';
-import useStyles from './styles'
+import {useDispatch} from 'react-redux';
+import useStyles from './styles';
+
+import {createPost} from '../../actions/posts'
 
 const Form = () => {
 
@@ -16,9 +19,12 @@ const Form = () => {
     })
 
     const classes = useStyles();
+    const dispatch = useDispatch();
 
-    const handleSubmit = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        dispatch(createPost(postData))
     }
     const clear = () => {
 
@@ -64,9 +70,7 @@ const Form = () => {
                     onChange={(e) => setPostData({ ...postData, tags: e.target.value })}
 
                 />
-                <div className={classes.fileInput}>
-                    <FileBase type="file" multiple="false" onDone={({base64})=>setPostData({...postData,selectedFile:base64})} />
-                </div>
+                <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
                 <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth >Submit</Button>
                 <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth >Clear</Button>
             </form>
